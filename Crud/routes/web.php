2e52ed636449c97b5437257d74f106281afd252b
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-26 21:26:56 -0300

    c

--- Crud/routes/web.php
+++ Crud/routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[UserController::class, 'index'])->name('user.index');
