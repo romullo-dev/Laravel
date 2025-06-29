6140310fd813b49f3cc6e349b87b13db8f8d41a5
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-29 15:25:39 -0300

    c-

--- meu-projeto/app/Http/Controllers/UserController.php
+++ meu-projeto/app/Http/Controllers/UserController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use PHPUnit\Framework\ExceptionWrapper;
 
 class UserController extends Controller
 {
-    public function create ()
+    public function create()
     {
-        return view ('create');
+        return view('create');
     }
 
-    public function store (Request $request)
+    public function store(Request $request)
     {
-        dd ($request);
+        try {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+            return redirect()->route('user.create')->with(
+                'success',
+                'Usuário cadastrado com sucesso!'
+            );
+        } catch (Exception $e) {
+            return back()->withInput()->with(
+                'error',
+                'Usuário não cadastrado!'
+            );
+        }
     }
 }
--- meu-projeto/resources/views/create.blade.php
+++ meu-projeto/resources/views/create.blade.php
@@ -1,34 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    @vite('')
+
+</head>
+
+<body>
+    <h1>Cadastrar usuário</h1>
+
+    @if (session('success'))
+        <p style="color : #086">
+            {{ session('success') }}
+        </p>
+    @endif
+    @if (session('error'))
+        <p style="color : rgb(136, 0, 0)">
+            {{ session('error') }}
+        </p>
+    @endif
+
+    <form action="{{ route('user.store') }}" method="post">
+        @csrf
+
+        <label for="name">Nome :</label>
+        <input type="text" name="name" id="name" placeholder="Digite seu nome" value="{{ old('name') }}" required>
+        <br><br><br>
+        <label for="email">E-mail :</label>
+        <input type="email" name="email" id="email" placeholder="Digite seu email" value="{{ old('email') }}"
+            required><br><br><br>
+
+        <label for="password">Senha :</label>
+        <input type="password" name="password" id="password" placeholder="Minino 6 caracteres"
+            value="{{ old('password') }}" required><br><br><br>
+
+        <button type="submit">Cadastrar</button>
+    </form>
 
 
-    </head>
-    <body>
-        <h1>Cadastrar usuário</h1>
 
-        <form action="{{ route('user.store') }}" method="post">
-            @csrf
+</body>
 
-            <label for="name">Nome :</label>
-            <input type="text" name="name" id="name" placeholder="Digite seu nome" value="{{ old('name') }}" required > <br><br><br>
-            <label for="email">E-mail :</label>
-            <input type="email" name="email" id="email" placeholder="Digite seu email" value="{{ old('email') }}" required ><br><br><br>
-
-            <label for="password">Senha :</label>
-            <input type="password" name="password" id="password" placeholder="Minino 6 caracteres" value="{{ old('password') }}" required ><br><br><br>
-            
-            <button type="submit">Cadastrar</button>
-        </form>
-
-
-
-    </body>
 </html>
