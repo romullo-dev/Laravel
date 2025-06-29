e88b75e7b581f4aad22842ccba51622863049847
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-29 02:49:09 -0300

    Commit

---
+++ meu-projeto/resources/views/create.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+    </head>
+    <body>
+        <h1>Cadastrar usuário</h1>
+
+        <form action="{{ route('user.store') }}" method="post">
+            @csrf
+
+            <label for="name">Nome :</label>
+            <input type="text" name="name" id="name" placeholder="Digite seu nome" value="{{ old('name') }}" required > <br><br><br>
+            <label for="email">E-mail :</label>
+            <input type="email" name="email" id="email" placeholder="Digite seu email" value="{{ old('email') }}" required ><br><br><br>
+
+            <label for="password">Senha :</label>
+            <input type="password" name="password" id="password" placeholder="Minino 6 caracteres" value="{{ old('password') }}" required ><br><br><br>
+            
+            <button type="submit">Cadastrar</button>
+        </form>
+
+
+
+    </body>
+</html>
---
+++ meu-projeto/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
+Route::post('/store-user', [UserController::class, 'store'])->name('user.store');
+
