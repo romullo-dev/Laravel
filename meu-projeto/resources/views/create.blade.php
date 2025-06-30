b7e1c38b559c391685eba79e16196c70a5453843
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-30 19:43:50 -0300

    C

--- meu-projeto/resources/views/create.blade.php
+++ meu-projeto/resources/views/create.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    @vite('')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
